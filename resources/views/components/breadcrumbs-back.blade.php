68a17f1ee3d63ea2ac63016a2722aae186863918
pop-culture-studio <pcs.engineer.team@gmail.com>
2021-12-15 14:38:39 +0900

    Update breadcrumbs-back.blade.php

--- resources/views/components/breadcrumbs-back.blade.php
+++ resources/views/components/breadcrumbs-back.blade.php
@@ -1,1 +1,1 @@
-<div class="py-3"><a href="{{ route('index') }}" class="text-indigo-500 hover:underline">{{ config('app.name') }}</a> &gt; <a href="{{ url()->previous() }}" class="text-indigo-500 hover:underline">戻る</a></div>
+<div class="p-3"><a href="{{ route('index') }}" class="text-indigo-500 hover:underline">{{ config('app.name') }}</a> &gt; <a href="{{ url()->previous() }}" class="text-indigo-500 hover:underline">戻る</a></div>
