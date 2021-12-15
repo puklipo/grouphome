b793ba473c41e7e352640e7921a2adc9e807f4a5
pop-culture-studio <pcs.engineer.team@gmail.com>
2021-12-15 14:12:30 +0900

    Update show.blade.php

--- resources/views/homes/show.blade.php
+++ resources/views/homes/show.blade.php
@@ -21,7 +21,7 @@
 
     <div class="py-6">
         <div class="sm:px-6 lg:px-8">
-            <div class="bg-white w-full m-6">
+            <div class="bg-white m-6">
                 <div class="mt-3">
                     <span class="bg-indigo-500 text-white px-6 py-1">
                         <a href="{{ route('pref', $home->pref) }}">
