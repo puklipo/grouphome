fd4e8170749d5a745a8310f90be07f1bcc1b772e
pop-culture-studio <pcs.engineer.team@gmail.com>
2021-12-15 14:09:02 +0900

    Update prefs.blade.php

--- resources/views/side/prefs.blade.php
+++ resources/views/side/prefs.blade.php
@@ -2,7 +2,7 @@
     @foreach(\App\Models\Pref::withCount('homes')->latest('homes_count')->get() as $pref)
         <li>
             <a href="{{ route('pref', $pref) }}"
-               class="block bg-white shadow-md rounded-md font-bold text-lg text-indigo-500 p-2 my-3 @if(request()->pref?->is($pref)) border-2 border-indigo-500 @endif">
+               class="block bg-white shadow-sm rounded-lg font-bold text-lg text-indigo-500 p-2 my-3 @if(request()->pref?->is($pref)) border-2 border-indigo-500 @endif">
                 {{ $pref->name }}
                 [{{ $pref->homes_count }}]
             </a>
