55e62299f1e7067ad80bc718d2614d15b0aa29ea
pop-culture-studio <pcs.engineer.team@gmail.com>
2021-12-15 12:49:23 +0900

    saga

---
+++ app/Imports/SagaImport.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Imports;
+
+use App\Imports\Concerns\WithImport;
+use App\Imports\Concerns\WithKana;
+use App\Models\Home;
+use App\Models\Pref;
+use Maatwebsite\Excel\Concerns\Importable;
+use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Maatwebsite\Excel\Concerns\WithUpserts;
+
+class SagaImport implements ToModel, WithHeadingRow, WithUpserts
+{
+    use Importable;
+    use WithImport;
+    use WithKana;
+
+    /**
+     * @param  array  $row
+     *
+     * @return \Illuminate\Database\Eloquent\Model|null
+     */
+    public function model(array $row)
+    {
+        if (empty($row['事業所番号'])) {
+            return null;
+        }
+
+        return new Home([
+            'id' => trim($row['事業所番号']),
+            'pref_id' => Pref::where('key', 'saga')->first()->id,
+            'name' => $this->kana($row['事業所名']),
+            'company' => $this->kana($row['法人(設置者)名']),
+            'address' => $this->kana('佐賀県'.$row['所在市町'].$row['所在地']),
+            'released_at' => $row['指定年月日'],
+        ]);
+    }
+}
