9a81ae3b82ae9d36636b61a3f01cc405335d05de
pop-culture-studio <pcs.engineer.team@gmail.com>
2021-12-20 14:50:46 +0900

    excel

--- app/Imports/AbstractImport.php
+++ app/Imports/AbstractImport.php
@@ -4,14 +4,12 @@ namespace App\Imports;
 
 use App\Imports\Concerns\WithImport;
 use App\Imports\Concerns\WithKana;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Maatwebsite\Excel\Concerns\Importable;
 use Maatwebsite\Excel\Concerns\ToModel;
-use Maatwebsite\Excel\Concerns\WithChunkReading;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 use Maatwebsite\Excel\Concerns\WithUpserts;
 
-abstract class AbstractImport implements ToModel, WithHeadingRow, WithUpserts, WithChunkReading, ShouldQueue
+abstract class AbstractImport implements ToModel, WithHeadingRow, WithUpserts
 {
     use Importable;
     use WithImport;
