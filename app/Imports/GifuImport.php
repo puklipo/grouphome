c376788fcde814cb15610d3ac8d5a52cdbd065c2
pop-culture-studio <pcs.engineer.team@gmail.com>
2021-12-20 12:37:37 +0900

    gifu

---
+++ app/Imports/GifuImport.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Imports;
+
+use App\Imports\Concerns\WithImport;
+use App\Imports\Concerns\WithKana;
+use App\Models\Home;
+use Maatwebsite\Excel\Concerns\Importable;
+use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Maatwebsite\Excel\Concerns\WithUpserts;
+
+class GifuImport implements ToModel, WithHeadingRow, WithUpserts
+{
+    use Importable;
+    use WithImport;
+    use WithKana;
+
+    /**
+     * @param  array  $row
+     * @return \Illuminate\Database\Eloquent\Model|null
+     */
+    public function model(array $row)
+    {
+        if (empty($row['事業者番号'])) {
+            return null;
+        }
+
+        return new Home([
+            'id' => $row['事業者番号'],
+            'pref_id' => $this->prefId(),
+            'name' => $this->kana($row['事業所名']),
+            'company' => $this->kana($row['法人名']),
+            'tel' => $this->kana($row['電話番号']),
+            'address' => '岐阜県'.$this->kana($row['事業所所在地']),
+            'map' => $row['Googleマップ'] ?? null,
+            'url' => $row['URL'] ?? null,
+            'released_at' => $row['指定年月日'],
+        ]);
+    }
+}
