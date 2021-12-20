87597b6befe2cf8723271acd0e3c61cbde5e7087
pop-culture-studio <pcs.engineer.team@gmail.com>
2021-12-20 13:35:39 +0900

    saitama

---
+++ app/Imports/SaitamaImport.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Imports;
+
+use App\Imports\Concerns\WithImport;
+use App\Imports\Concerns\WithKana;
+use App\Models\Home;
+use Maatwebsite\Excel\Concerns\Importable;
+use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Maatwebsite\Excel\Concerns\WithUpserts;
+use Illuminate\Support\Str;
+
+class SaitamaImport implements ToModel, WithHeadingRow, WithUpserts
+{
+    use Importable;
+    use WithImport;
+    use WithKana;
+
+    /**
+     * @param  array  $row
+     * @return \Illuminate\Database\Eloquent\Model|null
+     */
+    public function model(array $row)
+    {
+        if (empty($row['事業所番号'])) {
+            return null;
+        }
+
+        if (! Str::contains($row['異動年月日'], '/')) {
+            return null;
+        }
+
+        return new Home([
+            'id' => $row['事業所番号'].$row['枝番／ 連番'],
+            'pref_id' => $this->prefId(),
+            'name' => $this->kana($row['共同生活住居名称']),
+            'company' => $this->kana($row['申請者名称']),
+            'tel' => $this->kana($row['事業所電話番号']),
+            'address' => $this->kana($row['共同生活住居所在地']),
+            'map' => $row['Googleマップ'] ?? null,
+            'url' => $row['URL'] ?? null,
+            'released_at' => $row['異動年月日'],
+        ]);
+    }
+}
