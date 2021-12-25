6350526aafa44ee5587b54a684ad023ed21fe14c
pop-culture-studio <pcs.engineer.team@gmail.com>
2021-12-25 13:16:30 +0900

    Update item.blade.php

--- resources/views/homes/item.blade.php
+++ resources/views/homes/item.blade.php
@@ -1,13 +1,13 @@
 <div class="m-6">
     @include('homes.header')
 
-    <a href="{{ route('home.show', $home) }}">
-        <div class="border-4 border-indigo-500 p-3 dark:border-gray-800">
+    <div class="border-4 border-indigo-500 p-3 dark:border-gray-800">
+        <a href="{{ route('home.show', $home) }}">
             <h2 class="text-5xl text-indigo-500 dark:text-white font-extrabold tracking-widest hover:text-indigo-600 dark:hover:text-gray-500">
                 {{ $home->name }}
             </h2>
-            <div class="text-md my-3">{{ $home->address }}</div>
-        </div>
-    </a>
+        </a>
+        <div class="text-md my-3">{{ $home->address }}</div>
+    </div>
 
 </div>
