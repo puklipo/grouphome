a6d70a8fab1a7d1d1dfce5fcb6d59d6af99637a9
PCS <pcs.engineer.team@gmail.com>
2021-12-26 15:28:18 +0900

    wip

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -1,7 +1,7 @@
 <form action="{{ request()->routeIs(['pref', 'area']) ? url()->current() : route('index') }}"
       method="get">
     <x-jet-label for="search" value="{{ __('検索') }}" class="hidden"/>
-    <x-jet-input name="q" type="search" class="block flex-auto dark:bg-gray-800"
+    <x-jet-input name="q" type="search" class="block flex-auto min-w-max dark:bg-gray-800"
                  value="{{ request('q') }}"
                  placeholder="{{ request()->routeIs(['pref', 'area']) ? request()->pref->name.'から検索' : 'キーワード検索' }}"
     />
@@ -17,5 +17,5 @@
         @endforeach
     </select>
 
-    <x-jet-button class="block min-w-max mt-3" title="検索">検索</x-jet-button>
+    <x-jet-button class="block min-w-max w-full mt-3" title="検索">検索</x-jet-button>
 </form>
