ccafd4a4b109447311134dbbc7fbe165add37b16
PCS <pcs.engineer.team@gmail.com>
2022-01-01 13:29:56 +0900

    Update show.blade.php

--- resources/views/homes/show.blade.php
+++ resources/views/homes/show.blade.php
@@ -38,22 +38,6 @@
                     <div class="text-md my-3">{{ $home->company }}</div>
                     <div class="text-md my-3">{{ $home->tel }}</div>
 
-                    <div class="text-md my-3">対象区分
-                        @if($home->level > 0)
-                            {{ $home->level }} 以上
-                        @else
-                            区分なし以上、もしくは情報なし。
-                        @endif
-                    </div>
-
-                    @isset($home->type)
-                        <div class="text-md my-3">{{ $home->type->type }}</div>
-                    @endisset
-
-                    @if($home->trial)
-                        <div class="text-md my-3">体験利用必須</div>
-                    @endif
-
                     @if($home->furniture)
                         <div class="text-md my-3">家具付き</div>
                     @endif
@@ -77,6 +61,29 @@
                     @endisset
 
                     <div class="text-md my-3">指定年月日 {{ $home->released_at->toDateString() }}</div>
+                </div>
+
+                <div class="mt-6">
+                    <span class="bg-indigo-500 text-white px-6 py-1 dark:bg-gray-800">
+                          利用条件
+                    </span>
+                </div>
+                <div class="border-4 border-indigo-500 p-3 dark:border-gray-800">
+                    <div class="text-md my-3">対象区分
+                        @if($home->level > 0)
+                            {{ $home->level }} 以上
+                        @else
+                            区分なし以上、もしくは情報なし。
+                        @endif
+                    </div>
+
+                    @isset($home->type)
+                        <div class="text-md my-3">{{ $home->type->type }}</div>
+                    @endisset
+
+                    @if($home->trial)
+                        <div class="text-md my-3">体験利用必須</div>
+                    @endif
                 </div>
 
                 <div class="mt-6">
