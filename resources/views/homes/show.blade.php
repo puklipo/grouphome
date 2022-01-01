8e903c0761e601317d9d970286d3207a3e06c9f2
PCS <pcs.engineer.team@gmail.com>
2022-01-01 13:37:40 +0900

    Update show.blade.php

--- resources/views/homes/show.blade.php
+++ resources/views/homes/show.blade.php
@@ -38,10 +38,6 @@
                     <div class="text-md my-3">{{ $home->company }}</div>
                     <div class="text-md my-3">{{ $home->tel }}</div>
 
-                    @if($home->furniture)
-                        <div class="text-md my-3">家具付き</div>
-                    @endif
-
                     @isset($home->url)
                         <div class="text-md my-3">
                             <a href="{{ $home->url }}" target="_blank"
@@ -83,6 +79,17 @@
 
                     @if($home->trial)
                         <div class="text-md my-3">体験利用必須</div>
+                    @endif
+                </div>
+
+                <div class="mt-6">
+                    <span class="bg-indigo-500 text-white px-6 py-1 dark:bg-gray-800">
+                          設備
+                    </span>
+                </div>
+                <div class="border-4 border-indigo-500 p-3 dark:border-gray-800">
+                    @if($home->furniture)
+                        <div class="text-md my-3">家具付き</div>
                     @endif
                 </div>
 
