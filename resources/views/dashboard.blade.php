4b0a51ce911ad2225e8fb1e6a34c5c8bf3baa5c1
pop-culture-studio <pcs.engineer.team@gmail.com>
2022-01-05 10:45:32 +0900

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,12 +8,15 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <h2 class="text-4xl p-3">管理グループホーム</h2>
+                <h2 class="text-4xl p-3">管理グループホーム一覧</h2>
                 <ul>
                     @foreach($homes as $home)
-                        <li class="p-3">
-                            <a href="{{ route('home.show',$home) }}"
-                               class="text-xl text-indigo-500 dark:text-white font-bold hover:underline">{{ $home->name }}</a>
+                        <li class="p-3 flex justify-between">
+                            <a href="{{ route('home.show', $home) }}"
+                               class="text-xl text-indigo-500 dark:text-white font-bold hover:underline">
+                                {{ $home->name }}
+                            </a>
+                            <span class="text-md text-gray-500 ml-6">{{ Str::limit($home->address, 50) }}</span>
                         </li>
                     @endforeach
                 </ul>
