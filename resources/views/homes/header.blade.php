97ffeed48e37db8948f041c5f2e44fb873ced28c
pop-culture-studio <pcs.engineer.team@gmail.com>
2022-01-08 09:43:53 +0900

    Update header.blade.php

--- resources/views/homes/header.blade.php
+++ resources/views/homes/header.blade.php
@@ -5,7 +5,7 @@
             </a>
         </span>
     @if(filled($home->area))
-        <span class="bg-indigo-400 text-white px-6 py-1 dark:bg-gray-800 dark:text-white dark:hover:text-gray-500">
+        <span class="bg-indigo-500 text-white px-6 py-1 dark:bg-gray-800 dark:text-white dark:hover:text-gray-500">
                 <a href="{{ route('pref.area', [$home->pref, $home->area]) }}">
                     {{ $home->area }}
                 </a>
