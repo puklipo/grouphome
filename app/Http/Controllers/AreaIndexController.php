3afeb606ec72bd583ba6250d0dbeefddc5686b4f
PCS <pcs.engineer.team@gmail.com>
2022-01-14 12:24:47 +0900

    Update AreaIndexController.php

--- app/Http/Controllers/AreaIndexController.php
+++ app/Http/Controllers/AreaIndexController.php
@@ -7,21 +7,19 @@ use Illuminate\Http\Request;
 
 class AreaIndexController extends Controller
 {
-    /**
-     * Handle the incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function __invoke(Request $request)
     {
-        $areas = Home::with('pref')
-                     ->select(['pref_id', 'area'])
-                     ->whereNotNull('area')
-                     ->distinct()
-                     ->oldest('pref_id')
-                     ->get()
-                     ->groupBy('pref.name');
+        $areas = cache()->remember(
+            'area.index',
+            now()->addHour(),
+            fn () => Home::with('pref')
+                         ->select(['pref_id', 'area'])
+                         ->whereNotNull('area')
+                         ->distinct()
+                         ->oldest('pref_id')
+                         ->get()
+                         ->groupBy('pref.name')
+        );
 
         return view('area.index')->with(compact('areas'));
     }
