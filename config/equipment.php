73f2a21a9b012573b6ba6701be5eba9e965bae51
PCS <pcs.engineer.team@gmail.com>
2022-01-16 15:38:46 +0900

    add bed

--- config/equipment.php
+++ config/equipment.php
@@ -2,16 +2,17 @@
 
 //居室設備
 return [
-    'closet'        => 'クローゼット',
-    'aircon'        => 'エアコン',
-    'kitchen'       => 'キッチン',
-    'toilet'        => 'トイレ',
-    'bath'          => 'お風呂',
-    'shower'        => 'シャワールーム',
-    'wash'          => '洗濯機',
-    'tv'            => 'テレビ',
-    'internet'      => 'インターネット（有料）',
+    'closet' => 'クローゼット',
+    'aircon' => 'エアコン',
+    'kitchen' => 'キッチン',
+    'toilet' => 'トイレ',
+    'bath' => 'お風呂',
+    'shower' => 'シャワールーム',
+    'bed' => 'ベッド',
+    'tv' => 'テレビ',
+    'wash' => '洗濯機',
+    'internet' => 'インターネット（有料）',
     'internet_free' => 'インターネット（無料）',
-    'wifi'          => 'Wi-Fi',
-    'furniture'     => '家具付き',
+    'wifi' => 'Wi-Fi',
+    'furniture' => '家具付き',
 ];
