7e9a159b6c88017a717ae75cffc9e8ac39802d8e
PCS <pcs.engineer.team@gmail.com>
2022-01-18 10:11:39 +0900

    Update index-item.blade.php

--- resources/views/homes/index-item.blade.php
+++ resources/views/homes/index-item.blade.php
@@ -3,7 +3,7 @@
 
     <div class="border-4 border-indigo-500 dark:border-gray-800">
 
-        @if(Storage::exists($home->photo->cover))
+        @if(filled($home->photo->cover) && Storage::exists($home->photo->cover))
             <a href="{{ route('home.show', $home) }}">
                 <img src="{{ Storage::url($home->photo->cover) }}"
                      class="m-0 dark:grayscale w-full max-h-20 sm:max-h-36 object-cover object-center overflow-hidden hover:opacity-80"
