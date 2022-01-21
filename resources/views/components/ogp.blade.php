b990f030f286069d2796667b25707ee1cc31d6e0
PCS <pcs.engineer.team@gmail.com>
2022-01-21 11:35:20 +0900

    ogp

--- resources/views/components/ogp.blade.php
+++ resources/views/components/ogp.blade.php
@@ -2,4 +2,4 @@
 <meta name="twitter:creator" content="{{ $name ?? '@PCS_miraizu' }}">
 <meta property="og:title" content="{{ $title ?? config('app.name') }}">
 <meta property="og:description" content="{{ $description ?? '' }}">
-<meta property="og:image" content="{{ $image ?? config('grouphome.no_photo') }}">
+<meta property="og:image" content="{{ $image ?? asset('images/ogp.png') }}">
