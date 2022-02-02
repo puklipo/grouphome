c61570487e1daaf982bd39f9550ed8dd717e8c3d
PCS <pcs.engineer.team@gmail.com>
2022-02-02 15:11:21 +0900

    Update WamCommand.php

--- app/Console/Commands/WamCommand.php
+++ app/Console/Commands/WamCommand.php
@@ -38,38 +38,6 @@ class WamCommand extends Command
      */
     public function handle()
     {
-        //新着ページは取得できるので新着チェックくらいは可能。
-        $response = Http::asForm()->post('https://www.wam.go.jp/sfkohyoout/COP000102E01.do', [
-            '_FRAMEID' => 'root',
-            '_TARGETID' => 'root',
-            '_LUID' => '',
-            '_TOKEN' => now()->timestamp,
-            '_FORMID' => 'COP000102',
-            '_SUBINDEX' => '',
-            'vo_headVO_prefCode' => $this->argument('id'),
-        ]);
-        dd($response->body());
-
-        //        Http::get('https://www.wam.go.jp/sfkohyoout/COP000100E0000.do');
-
-        //事業者番号で直接取得はまだできない。
-        //        $response = Http::asForm()->dump()->post('https://www.wam.go.jp/sfkohyoout/COP000100E12.do', [
-        //            '_FRAMEID' => 'root',
-        //            '_TARGETID' => 'root',
-        //            '_LUID' => '',
-        //            '_TOKEN' => now()->subMinute()->timestamp,
-        //            '_FORMID' => 'COP000100',
-        //            '_SUBINDEX' => '',
-        //            'vo_headVO_adress' => '',
-        //            'vo_headVO_corporation_name' => '',
-        //            'vo_headVO_facility_name' => '',
-        //            'vo_headVO_no' => $this->argument('id'),
-        //            'vo_headVO_townCode' => '',
-        //            'vo_headVO_prefCode' => '',
-        //        ]);
-
-        //dd($response->body(), now()->timestamp);
-
         return 0;
     }
 }
