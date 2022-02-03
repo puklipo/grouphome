4f678b4de43e98fe6ab899be443141dcf30896cc
PCS <pcs.engineer.team@gmail.com>
2022-02-03 19:15:47 +0900

    Update SagaImport.php

--- app/Imports/SagaImport.php
+++ app/Imports/SagaImport.php
@@ -3,6 +3,7 @@
 namespace App\Imports;
 
 use App\Models\Home;
+use Illuminate\Support\Str;
 
 class SagaImport extends AbstractImport
 {
@@ -19,12 +20,15 @@ class SagaImport extends AbstractImport
         return new Home([
             'id' => $this->kana(trim($row['事業所番号'])),
             'pref_id' => $this->prefId(),
-            'name' => $this->kana($row['事業所名']),
-            'company' => $this->kana($row['法人(設置者)名']),
-            'tel' => $this->kana($row['電話番号']),
-            'address' => $this->kana('佐賀県'.$row['所在市町'].$row['所在地']),
-            'area' => $this->kana($row['所在市町']),
-            'released_at' => $this->kana($row['指定年月日']),
+            'name' => $this->kana($row['事業所の名称']),
+            'company' => $this->kana($row['法人の名称']),
+            'tel' => $this->kana($row['事業所電話番号']),
+            'address' => $this->kana($row['事業所住所（市区町村）'].$row['事業所住所（番地以降）']),
+            'area' => $this->kana(Str::remove('佐賀県', $row['事業所住所（市区町村）'])),
+            'url' => $row['事業所URL'],
+            'level' => $this->kana($row['対象区分'] ?? 0),
+            'type_id' => $row['類型'] ?? null,
+            'released_at' => $row['指定年月日'] ?? null,
         ]);
     }
 }
