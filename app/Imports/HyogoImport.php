54960c920f1eac9e4e1656d86181bf48f1fa4caa
PCS <pcs.engineer.team@gmail.com>
2022-02-03 18:39:49 +0900

    Update HyogoImport.php

--- app/Imports/HyogoImport.php
+++ app/Imports/HyogoImport.php
@@ -3,6 +3,7 @@
 namespace App\Imports;
 
 use App\Models\Home;
+use Illuminate\Support\Str;
 
 class HyogoImport extends AbstractImport
 {
@@ -17,15 +18,17 @@ class HyogoImport extends AbstractImport
         }
 
         return new Home([
-            'id' => $this->kana($row['事業所番号']),
+            'id' => $this->kana(trim($row['事業所番号'])),
             'pref_id' => $this->prefId(),
-            'name' => $this->kana($row['事業所－名称']),
-            'company' => $this->kana($row['申請者－名称']),
-            'tel' => $this->kana($row['事業所－電話番号']),
-            'address' => $this->kana($row['事業所－住所']),
-            'map' => $row['Googleマップ'] ?? null,
-            'url' => $row['URL'] ?? null,
-            'released_at' => $this->kana($row['指定年月日']),
+            'name' => $this->kana($row['事業所の名称']),
+            'company' => $this->kana($row['法人の名称']),
+            'tel' => $this->kana($row['事業所電話番号']),
+            'address' => $this->kana($row['事業所住所（市区町村）'].$row['事業所住所（番地以降）']),
+            'area' => $this->kana(Str::remove('兵庫県', $row['事業所住所（市区町村）'])),
+            'url' => $row['事業所URL'],
+            'level' => $this->kana($row['対象区分'] ?? 0),
+            'type_id' => $row['類型'] ?? null,
+            'released_at' => $row['指定年月日'] ?? null,
         ]);
     }
 }
