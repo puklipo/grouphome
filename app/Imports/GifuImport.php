f64a6bd56c4a493c092c46f6cbad7ccb740738d6
PCS <pcs.engineer.team@gmail.com>
2022-02-03 19:15:49 +0900

    Update GifuImport.php

--- app/Imports/GifuImport.php
+++ app/Imports/GifuImport.php
@@ -3,6 +3,7 @@
 namespace App\Imports;
 
 use App\Models\Home;
+use Illuminate\Support\Str;
 
 class GifuImport extends AbstractImport
 {
@@ -12,20 +13,22 @@ class GifuImport extends AbstractImport
      */
     public function model(array $row)
     {
-        if (empty($row['事業者番号'])) {
+        if (empty($row['事業所番号'])) {
             return null;
         }
 
         return new Home([
-            'id' => $this->kana($row['事業者番号']),
+            'id' => $this->kana(trim($row['事業所番号'])),
             'pref_id' => $this->prefId(),
-            'name' => $this->kana($row['事業所名']),
-            'company' => $this->kana($row['法人名']),
-            'tel' => $this->kana($row['電話番号']),
-            'address' => '岐阜県'.$this->kana($row['事業所所在地']),
-            'map' => $row['Googleマップ'] ?? null,
-            'url' => $row['URL'] ?? null,
-            'released_at' => $this->kana($row['指定年月日']),
+            'name' => $this->kana($row['事業所の名称']),
+            'company' => $this->kana($row['法人の名称']),
+            'tel' => $this->kana($row['事業所電話番号']),
+            'address' => $this->kana($row['事業所住所（市区町村）'].$row['事業所住所（番地以降）']),
+            'area' => $this->kana(Str::remove('岐阜県', $row['事業所住所（市区町村）'])),
+            'url' => $row['事業所URL'],
+            'level' => $this->kana($row['対象区分'] ?? 0),
+            'type_id' => $row['類型'] ?? null,
+            'released_at' => $row['指定年月日'] ?? null,
         ]);
     }
 }
