1153c405c59f503377b73eb1f9512c9bd2c81eda
PCS <pcs.engineer.team@gmail.com>
2022-02-03 19:11:20 +0900

    Update AichiImport.php

--- app/Imports/AichiImport.php
+++ app/Imports/AichiImport.php
@@ -3,6 +3,7 @@
 namespace App\Imports;
 
 use App\Models\Home;
+use Illuminate\Support\Str;
 
 class AichiImport extends AbstractImport
 {
@@ -12,19 +13,22 @@ class AichiImport extends AbstractImport
      */
     public function model(array $row)
     {
-        if (empty($row['障害福祉サービス等事業所番号'])) {
+        if (empty($row['事業所番号'])) {
             return null;
         }
 
         return new Home([
-            'id' => $this->kana($row['障害福祉サービス等事業所番号']),
+            'id' => $this->kana(trim($row['事業所番号'])),
             'pref_id' => $this->prefId(),
-            'name' => $this->kana($row['施設・サービス名']),
+            'name' => $this->kana($row['事業所の名称']),
             'company' => $this->kana($row['法人の名称']),
-            'tel' => $this->kana($row['電話番号']),
-            'address' => $this->kana($row['所在地']),
-            'area' => '名古屋市'.$this->kana($row['タグ']).'区',
-            'released_at' => $this->kana($row['指定年月日']),
+            'tel' => $this->kana($row['事業所電話番号']),
+            'address' => $this->kana($row['事業所住所（市区町村）'].$row['事業所住所（番地以降）']),
+            'area' => $this->kana(Str::remove('愛知県', $row['事業所住所（市区町村）'])),
+            'url' => $row['事業所URL'],
+            'level' => $this->kana($row['対象区分'] ?? 0),
+            'type_id' => $row['類型'] ?? null,
+            'released_at' => $row['指定年月日'] ?? null,
         ]);
     }
 }
