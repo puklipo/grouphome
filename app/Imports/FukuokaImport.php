6bbe41d7612a4eb29baec5e5f33484d8b05d9b79
PCS <pcs.engineer.team@gmail.com>
2022-02-04 20:53:09 +0900

    Update FukuokaImport.php

--- app/Imports/FukuokaImport.php
+++ app/Imports/FukuokaImport.php
@@ -3,6 +3,7 @@
 namespace App\Imports;
 
 use App\Models\Home;
+use Illuminate\Support\Str;
 
 class FukuokaImport extends AbstractImport
 {
@@ -17,17 +18,15 @@ class FukuokaImport extends AbstractImport
         }
 
         return new Home([
-            'id' => $this->kana($row['事業所番号']),
+            'id' => $this->kana(trim($row['事業所番号'])),
             'pref_id' => $this->prefId(),
-            'name' => $this->kana($row['事業所－名称']),
-            'company' => $this->kana($row['申請者－名称']),
-            'tel' => $this->kana($row['事業所－電話番号']),
-            'address' => $this->kana($row['事業所－住所']),
-            'area' => $this->kana($row['市区町村']),
-            'map' => $row['Googleマップ'] ?? null,
-            'url' => $row['URL'] ?? null,
-            'wam' => $row['WAM'] ?? null,
-            'released_at' => $this->kana($row['指定年月日']),
+            'name' => $this->kana($row['事業所の名称']),
+            'company' => $this->kana($row['法人の名称']),
+            'tel' => $this->kana($row['事業所電話番号']),
+            'address' => $this->kana($row['事業所住所（市区町村）'].$row['事業所住所（番地以降）']),
+            'area' => $this->kana(Str::remove('福岡県', $row['事業所住所（市区町村）'])),
+            'url' => $row['事業所URL'],
+            'released_at' => $row['指定年月日'] ?? null,
         ]);
     }
 }
