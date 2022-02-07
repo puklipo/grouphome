f3cfaa548966ea51f7824d9846709f4b214801de
PCS <pcs.engineer.team@gmail.com>
2022-02-07 17:36:01 +0900

    Update cost.php

--- config/cost.php
+++ config/cost.php
@@ -6,7 +6,7 @@ return [
     'rent' => '家賃',
     'food' => '食費',
     'utilities' => '水道・光熱費',
-    'daily' => '日用品・雑費',
+    'daily' => '日用品・雑費・共益費',
     'etc' => 'その他',
     'support' => '家賃補助',
     'message' => '補足説明',
