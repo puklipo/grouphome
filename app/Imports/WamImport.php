3392d2ae56ce8923b88c0dff58f3449423dca943
PCS <pcs.engineer.team@gmail.com>
2022-02-18 21:45:49 +0900

    Update WamImport.php

--- app/Imports/WamImport.php
+++ app/Imports/WamImport.php
@@ -16,13 +16,14 @@ use Maatwebsite\Excel\Concerns\WithValidation;
 use Maatwebsite\Excel\Row;
 use Maatwebsite\Excel\Validators\Failure;
 
-class WamImport implements OnEachRow,
-                           WithHeadingRow,
-                           WithBatchInserts,
-                           WithChunkReading,
-                           SkipsEmptyRows,
-                           WithValidation,
-                           SkipsOnFailure
+class WamImport implements
+    OnEachRow,
+    WithHeadingRow,
+    WithBatchInserts,
+    WithChunkReading,
+    SkipsEmptyRows,
+    WithValidation,
+    SkipsOnFailure
 {
     use Importable;
     use WithKana;
