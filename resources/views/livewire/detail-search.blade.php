575722a8aa642fe86ef862a75bd9f0dcf26ed19e
pop-culture-studio <pcs.engineer.team@gmail.com>
2022-03-10 13:46:07 +0900

    Update detail-search.blade.php

--- resources/views/livewire/detail-search.blade.php
+++ resources/views/livewire/detail-search.blade.php
@@ -11,44 +11,6 @@
                      placeholder="{{ 'キーワード検索' }}"
         />
 
-        <h3 class="text-lg font-bold my-3">{{ __('都道府県') }}</h3>
-
-        <select name="pref_id"
-                wire:model="pref_id"
-                class="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm block flex-auto dark:bg-gray-800">
-            <option value="">指定しない</option>
-            @foreach(\App\Models\Pref::oldest('id')->get() as $pre)
-                <option value="{{ $pre->id }}">{{ $pre->name }}</option>
-            @endforeach
-        </select>
-
-        @if(count($areas ?? []) > 0)
-            <select name="area"
-                    wire:model="area"
-                    class="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm block flex-auto dark:bg-gray-800">
-                <option value="">指定しない</option>
-                @foreach($areas as $area_name)
-                    <option value="{{ $area_name }}">{{ $area_name }}</option>
-                @endforeach
-            </select>
-        @endif
-
-        <h3 class="text-lg font-bold my-3">{{ __('並べ替え') }}</h3>
-
-        <select name="sort"
-                wire:model="sort"
-                class="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm block flex-auto dark:bg-gray-800">
-            <option value="">なし</option>
-            <option value="updated">更新が新しい順</option>
-            <option value="low">費用が安い順</option>
-            <option value="high">費用が高い順</option>
-            <option value="address">住所</option>
-            <option value="release">指定年月日(新着順)</option>
-            <option value="name">グループホーム名</option>
-            <option value="pref">都道府県(北から)</option>
-            <option value="id">事業所番号(降順)</option>
-        </select>
-
         <h3 class="text-lg font-bold my-3">対象区分</h3>
         <p class="text-sm text-gray-500 mb-2"></p>
 
@@ -87,6 +49,44 @@
             <option value="">指定しない</option>
             <option value="0">{{ __('空室あり') }}</option>
             <option value="1">{{ __('満室') }}</option>
+        </select>
+
+        <h3 class="text-lg font-bold my-3">{{ __('都道府県') }}</h3>
+
+        <select name="pref_id"
+                wire:model="pref_id"
+                class="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm block flex-auto dark:bg-gray-800">
+            <option value="">指定しない</option>
+            @foreach(\App\Models\Pref::oldest('id')->get() as $pre)
+                <option value="{{ $pre->id }}">{{ $pre->name }}</option>
+            @endforeach
+        </select>
+
+        @if(count($areas ?? []) > 0)
+            <select name="area"
+                    wire:model="area"
+                    class="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm block flex-auto dark:bg-gray-800">
+                <option value="">指定しない</option>
+                @foreach($areas as $area_name)
+                    <option value="{{ $area_name }}">{{ $area_name }}</option>
+                @endforeach
+            </select>
+        @endif
+
+        <h3 class="text-lg font-bold my-3">{{ __('並べ替え') }}</h3>
+
+        <select name="sort"
+                wire:model="sort"
+                class="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm block flex-auto dark:bg-gray-800">
+            <option value="">なし</option>
+            <option value="updated">更新が新しい順</option>
+            <option value="low">費用が安い順</option>
+            <option value="high">費用が高い順</option>
+            <option value="address">住所</option>
+            <option value="release">指定年月日(新着順)</option>
+            <option value="name">グループホーム名</option>
+            <option value="pref">都道府県(北から)</option>
+            <option value="id">事業所番号(降順)</option>
         </select>
     </div>
 
