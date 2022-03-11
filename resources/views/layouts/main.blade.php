5c4c19771e7cbcd9e61afa84a93e8475d1418b38
pop-culture-studio <pcs.engineer.team@gmail.com>
2022-03-11 11:37:16 +0900

    Update main.blade.php

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -16,10 +16,10 @@
     @endisset
 
     <!-- Fonts -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;500;600;700;800;900&display=swap">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;500;600;700;800;900&display=swap" media="print" onload="this.media='all'">
 
     <!-- Styles -->
-    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}" media="print" onload="this.media='all'">
 
     @livewireStyles
 
