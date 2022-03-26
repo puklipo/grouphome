58d0d1b2076573161d76c8b459ecba4739131304
pop-culture-studio <pcs.engineer.team@gmail.com>
2022-03-26 10:45:45 +0900

    Update prepare.blade.php

--- resources/views/livewire/mail/prepare.blade.php
+++ resources/views/livewire/mail/prepare.blade.php
@@ -5,12 +5,13 @@
         <x-jet-validation-errors class="mb-4"/>
 
         <form wire:submit.prevent="sendmail">
-            <p class="mb-2 text-lg text-red-500 dark:text-gray-300">メールアドレスの入力ミスを防ぐため、最初にあなたのメールアドレスを入力してください。問い合わせフォームはメールで送られます。</p>
+            <p class="mb-2 text-lg text-red-500 dark:text-gray-300">
+                メールアドレスの入力ミスを防ぐため、最初にあなたのメールアドレスを入力してください。問い合わせフォームはメールで送られます。</p>
 
             <div class="mt-4">
                 <x-jet-label for="email" value="{{ __('Email') }}"/>
                 <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" wire:model.lazy="email"
-                             required autocomplete="email"/>
+                             required autofocus autocomplete="email"/>
             </div>
 
             <div class="flex items-center justify-end mt-4">
