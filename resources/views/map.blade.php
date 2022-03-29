5a8aaf6f423454f311495ef67a2d81c1c53f0d43
pop-culture-studio <pcs.engineer.team@gmail.com>
2022-03-29 14:30:05 +0900

    Update map.blade.php

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -1,3 +1,3 @@
 <div id="map" class="w-full sm:w-1/2 m-0 sm:m-3 p-6 bg-indigo-100 border border-indigo-500">
-    {!! File::get(resource_path('js/japanese-prefectures/map-mobile.svg')) !!}
+    {!! File::get(base_path('resources/js/japanese-prefectures/map-mobile.svg')) !!}
 </div>
