5c41bf266ffd868304bf7beea489724bc7dd35ce
pop-culture-studio <pcs.engineer.team@gmail.com>
2022-03-30 13:31:36 +0900

    icon

--- resources/views/mobile-menu.blade.php
+++ resources/views/mobile-menu.blade.php
@@ -6,7 +6,7 @@
         <div class="text-center text-white text-xl font-bold"><x-icon.location class="inline"></x-icon.location>自治体一覧</div>
     </a>
     <a href="{{ route('history') }}" class="p-6 bg-indigo-500 dark:bg-gray-800 rounded-md rounded-md">
-        <div class="text-center text-white text-xl font-bold"><x-icon.collection class="inline"></x-icon.collection>履歴</div>
+        <div class="text-center text-white text-xl font-bold"><x-icon.clock class="inline"></x-icon.clock>履歴</div>
     </a>
     <a href="{{ route('help.user') }}" class="p-6 bg-indigo-500 dark:bg-gray-800 rounded-md rounded-md">
         <div class="text-center text-white text-xl font-bold"><x-icon.question class="inline"></x-icon.question>ヘルプ</div>
