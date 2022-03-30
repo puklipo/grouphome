ad1dd1c94dc5f25c2c8f542d0c33c1666a1afd8b
pop-culture-studio <pcs.engineer.team@gmail.com>
2022-03-30 13:49:05 +0900

    nav

--- resources/views/mobile-menu.blade.php
+++ resources/views/mobile-menu.blade.php
@@ -1,4 +1,4 @@
-<div class="grid grid-cols-2 gap-2">
+<nav class="grid grid-cols-2 gap-2" aria-labelledby="mobile navigation">
     <a href="{{ route('detail-search') }}" class="p-6 bg-indigo-500 dark:bg-gray-800 rounded-md rounded-md">
         <div class="text-center text-white text-xl font-bold"><x-icon.search class="inline"></x-icon.search>検索</div>
     </a>
@@ -11,4 +11,4 @@
     <a href="{{ route('help.user') }}" class="p-6 bg-indigo-500 dark:bg-gray-800 rounded-md rounded-md">
         <div class="text-center text-white text-xl font-bold"><x-icon.question class="inline"></x-icon.question>ヘルプ</div>
     </a>
-</div>
+</nav>
