fce77e2a43a77a29db98458c3bfa6d2fe14bb8dc
pop-culture-studio <pcs.engineer.team@gmail.com>
2022-03-30 10:40:01 +0900

    Update map.js

--- resources/js/map.js
+++ resources/js/map.js
@@ -4,12 +4,12 @@ import 'svg-japan/dist/svg-japan.min'
 
 document.addEventListener( 'DOMContentLoaded', function() {
     svgJapan({
-        element: "#map",
-        type: "deform",
+        element: '#map',
+        type: 'deform',
         regionality: true,
-        activeColor: "#3b82f6",
-        width: "100%",
-        height: "100%"
+        activeColor: '#3b82f6',
+        width: '100%',
+        height: '100%'
     })
 }, false)
 
