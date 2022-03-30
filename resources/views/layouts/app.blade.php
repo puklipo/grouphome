e03ab820c62f1b6edf404a5caa26f4cfd17fe084
pop-culture-studio <pcs.engineer.team@gmail.com>
2022-03-30 13:54:52 +0900

    wip

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,13 +25,13 @@
             @livewire('navigation-menu')
 
             <!-- Page Heading -->
-            @if (isset($header))
+            @isset($header)
                 <header class="bg-white dark:bg-black text-black dark:text-white shadow">
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                         {{ $header }}
                     </div>
                 </header>
-            @endif
+            @endisset
 
             <!-- Page Content -->
             <main>
