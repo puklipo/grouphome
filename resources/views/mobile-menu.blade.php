db2fc2e456217bc966a15de5a07b10571d72dfea
pop-culture-studio <pcs.engineer.team@gmail.com>
2022-03-31 11:25:41 +0900

    wip

--- resources/views/mobile-menu.blade.php
+++ resources/views/mobile-menu.blade.php
@@ -1,14 +1,14 @@
 <nav class="grid grid-cols-2 gap-2" aria-labelledby="mobile navigation">
-    <a href="{{ route('detail-search') }}" class="p-6 bg-indigo-500 dark:bg-gray-800 rounded-md rounded-md">
+    <a href="{{ route('detail-search') }}" class="p-6 bg-indigo-500 dark:bg-gray-800 bg-gradient-to-b from-indigo-600 to-indigo-400 rounded-md rounded-md">
         <div class="text-center text-white text-xl font-bold"><x-icon.search class="inline"></x-icon.search>検索</div>
     </a>
-    <a href="{{ route('area.index') }}" class="p-6 bg-indigo-500 dark:bg-gray-800 rounded-md rounded-md">
+    <a href="{{ route('area.index') }}" class="p-6 bg-indigo-500 dark:bg-gray-800 bg-gradient-to-b from-indigo-600 to-indigo-400 rounded-md rounded-md">
         <div class="text-center text-white text-xl font-bold"><x-icon.location class="inline"></x-icon.location>自治体一覧</div>
     </a>
-    <a href="{{ route('history') }}" class="p-6 bg-indigo-500 dark:bg-gray-800 rounded-md rounded-md">
+    <a href="{{ route('history') }}" class="p-6 bg-indigo-500 dark:bg-gray-800 bg-gradient-to-b from-indigo-600 to-indigo-400 rounded-md rounded-md">
         <div class="text-center text-white text-xl font-bold"><x-icon.clock class="inline"></x-icon.clock>履歴</div>
     </a>
-    <a href="{{ route('help.user') }}" class="p-6 bg-indigo-500 dark:bg-gray-800 rounded-md rounded-md">
+    <a href="{{ route('help.user') }}" class="p-6 bg-indigo-500 dark:bg-gray-800 bg-gradient-to-b from-indigo-600 to-indigo-400 rounded-md rounded-md">
         <div class="text-center text-white text-xl font-bold"><x-icon.question class="inline"></x-icon.question>ヘルプ</div>
     </a>
 </nav>
