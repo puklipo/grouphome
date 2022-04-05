77d0162929ed4b7559a5f0ec13b9791e10011a4d
pop-culture-studio <pcs.engineer.team@gmail.com>
2022-04-05 13:30:01 +0900

    prefs

--- resources/views/side/prefs.blade.php
+++ resources/views/side/prefs.blade.php
@@ -4,7 +4,7 @@
     @foreach($prefs as $pref)
         <li>
             <a href="{{ route('pref', $pref) }}"
-               class="block bg-white shadow-sm rounded-md font-bold text-lg text-indigo-500 hover:text-indigo-600 p-2 my-3 dark:bg-gray-800 dark:text-white dark:hover:text-white dark:hover:bg-gray-700 @if(request()->pref?->is($pref)) border-2 border-indigo-500 dark:border-gray-500 @endif">
+               class="block bg-white shadow-sm rounded-md font-bold text-lg text-indigo-500 hover:ring-indigo-600 dark:hover:ring-gray-500 hover:ring-1 p-2 my-3 dark:bg-gray-800 dark:text-white dark:hover:text-white dark:hover:bg-gray-700 @if(request()->pref?->is($pref)) ring-1 dark:ring-2 ring-indigo-500 dark:ring-gray-500 @endif">
                 {{ $pref->name }}
                 [{{ $pref->homes_count }}]
             </a>
