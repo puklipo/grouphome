8e0eed6f76a12334633351daf57b9f9fbcc78da6
PCS <pcs.engineer.team@gmail.com>
2022-05-25 09:44:31 +0900

    Create 4020800076.blade.php

---
+++ resources/views/report/4020800076.blade.php
@@ -0,0 +1,17 @@
+<x-main-layout>
+    <x-slot name="title">
+        体験レポート スターホーム六本松（福岡県福岡市中央区）
+    </x-slot>
+
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl leading-tight">
+            体験レポート スターホーム六本松（福岡県福岡市中央区）
+        </h2>
+    </x-slot>
+
+    <div class="py-6">
+        <div class="px-3 sm:px-6 lg:px-8">
+
+        </div>
+    </div>
+</x-main-layout>
