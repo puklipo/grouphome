31a05d87d716e2b1b10bf9f3373e228126e1ad33
pop-culture-studio <pcs.engineer.team@gmail.com>
2022-09-15 10:25:32 +0900

    Update operator-home.blade.php

--- resources/views/admin/operator-home.blade.php
+++ resources/views/admin/operator-home.blade.php
@@ -22,8 +22,9 @@
                             <ul class="ml-10">
                                 @foreach($user->homes as $home)
                                     <li class="p-3 flex justify-between">
-                                        <a href="{{ route('home.show', $home) }}" class="text-xl text-indigo-500 dark:text-white font-bold hover:underline">
-                                        {{ $home->name }}
+                                        <a href="{{ route('home.show', $home) }}"
+                                           class="text-xl text-indigo-500 dark:text-white font-bold hover:underline">
+                                            {{ $home->name }}
                                         </a>
                                         <form action="{{ route('operator-home.destroy',[$user, $home]) }}"
                                               method="post"
