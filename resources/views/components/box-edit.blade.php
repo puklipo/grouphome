65a2e39cb30c7060d339df8f995ee5967d75a107
pop-culture-studio <pcs.engineer.team@gmail.com>
2022-09-21 12:36:38 +0900

    Update box-edit.blade.php

--- resources/views/components/box-edit.blade.php
+++ resources/views/components/box-edit.blade.php
@@ -2,7 +2,7 @@
     <span class="bg-red-500 text-white px-6 pb-1">
         {{ $title ?? '変更' }}
     </span>
-    <div {{ $attributes->class(['border-4 border-red-500 p-3']) }}>
+    <div {{ $attributes->class(['border-2 border-red-500 p-3']) }}>
         {{ $slot }}
     </div>
 </div>
