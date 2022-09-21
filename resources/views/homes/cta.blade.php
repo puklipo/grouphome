bd00e4a9326a37d4d7c5568a440b6265b45e2173
pop-culture-studio <pcs.engineer.team@gmail.com>
2022-09-21 11:26:02 +0900

    Update cta.blade.php

--- resources/views/homes/cta.blade.php
+++ resources/views/homes/cta.blade.php
@@ -1,11 +1,6 @@
 @guest
     @if($home->users()->doesntExist())
         <div class="print:hidden">
-            <div class="mt-6">
-            <span class="bg-red-500 text-white px-3 py-1 font-bold">
-                グループホーム事業者からの情報を募集しています
-            </span>
-            </div>
             <div class="border-4 border-red-500 p-3">
                 グループホームの詳細情報を入力するには
                 <a href="{{ route('help.operator') }}"
