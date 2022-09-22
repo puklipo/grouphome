105027a74ed2516bf7bbca25907cddd44e86ba4d
PCS <pcs.engineer.team@gmail.com>
2022-09-22 14:09:41 +0900

    wip

--- resources/views/components/box.blade.php
+++ resources/views/components/box.blade.php
@@ -1,1 +1,1 @@
-<div {{ $attributes->class(['border-2 border-indigo-500 dark:border-gray-800 print:border-1']) }}>{{ $slot }}</div>
+<div {{ $attributes->class(['border border-indigo-500 dark:border-gray-800 print:border']) }}>{{ $slot }}</div>
