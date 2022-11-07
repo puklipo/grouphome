6d287c26843becac8ab991ad15d04086ad197659
pop-culture-studio <pcs.engineer.team@gmail.com>
2022-11-07 14:45:02 +0900

    wip

--- resources/views/components/box.blade.php
+++ resources/views/components/box.blade.php
@@ -1,1 +1,1 @@
-<div {{ $attributes->class(['shadow-sm dark:shadow-lg dark:shadow-gray-800 border border-indigo-500 dark:border-gray-800 print:border print:shadow-none']) }}>{{ $slot }}</div>
+<div {{ $attributes->class(['shadow-sm hover:shadow-lg dark:shadow-sm dark:shadow-gray-800 border border-indigo-500 dark:border-gray-800 print:border print:shadow-none']) }}>{{ $slot }}</div>
