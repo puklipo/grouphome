15481568e76db68787a28156a45f0388701d83c6
pop-culture-studio <pcs.engineer.team@gmail.com>
2023-01-20 15:10:12 +0900

    Update location-index.blade.php

--- resources/views/livewire/location-index.blade.php
+++ resources/views/livewire/location-index.blade.php
@@ -10,6 +10,8 @@
     @forelse($homes as $home)
         @include('homes.index-item')
     @empty
+        <div class="p-3 font-bold">表示中...しばらく待っても表示されないときはブラウザを再読み込みしてください。</div>
+
         @unless($geo)
             <div class="p-3 font-bold text-red-500">ブラウザで位置情報へのアクセスを許可してください。</div>
         @endunless
