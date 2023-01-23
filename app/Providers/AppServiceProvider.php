978d627b0f5d0ffcec5faf97f352e13f3bf241a5
pop-culture-studio <pcs.engineer.team@gmail.com>
2023-01-23 11:04:31 +0900

    add ViewServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,11 +3,8 @@
 namespace App\Providers;
 
 use App\Doctrine\Geometry;
-use App\View\Composers\SearchComposer;
-use App\View\Composers\SideComposer;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -36,8 +33,5 @@ class AppServiceProvider extends ServiceProvider
         }
 
         Model::preventLazyLoading(! $this->app->isProduction());
-
-        View::composer('side', SideComposer::class);
-        View::composer('search.simple', SearchComposer::class);
     }
 }
