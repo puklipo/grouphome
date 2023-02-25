527cf93e70e80084ab46502c4034314fb8a69db7
pop-culture-studio <pcs.engineer.team@gmail.com>
2023-02-25 11:03:33 +0900

    Update AuthServiceProvider.php

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -24,8 +24,6 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        $this->registerPolicies();
-
         Gate::define('admin', function (User $user) {
             return $user->id === 1;
         });
