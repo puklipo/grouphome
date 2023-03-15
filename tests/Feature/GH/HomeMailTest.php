b7ef4782a30d110275c0faee436acdaa929fa14d
pop-culture-studio <pcs.engineer.team@gmail.com>
2023-03-15 13:18:59 +0900

    Update HomeMailTest.php

--- tests/Feature/GH/HomeMailTest.php
+++ tests/Feature/GH/HomeMailTest.php
@@ -3,7 +3,12 @@
 namespace Tests\Feature\GH;
 
 use App\Models\Home;
+use App\Notifications\HomeMailCreatedNotification;
+use App\Notifications\HomeMailNotification;
+use App\Notifications\MailPrepareNotification;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Notification;
+use Livewire\Livewire;
 use Tests\TestCase;
 
 class HomeMailTest extends TestCase
@@ -42,5 +47,35 @@ class HomeMailTest extends TestCase
         $response->assertOk()
                  ->assertViewHas('mail', 'test@example.com')
                  ->assertSeeLivewire('mail.form');
+    }
+
+    public function test_home_mail_prepare_sendmail()
+    {
+        Notification::fake();
+
+        $home = Home::factory()->create();
+
+        Livewire::test('mail.prepare', ['home' => $home])
+            ->set('email', 'test@localhost')
+            ->call('sendmail');
+
+        Notification::assertSentOnDemand(MailPrepareNotification::class);
+    }
+
+    public function test_home_mail_sendmail()
+    {
+        Notification::fake();
+
+        $home = Home::factory()->hasUsers(1)->create();
+
+        Livewire::test('mail.form', ['home' => $home, 'email' => 'test@localhost'])
+                ->set('name', 'test')
+                ->set('tel', 'test')
+                ->set('subject', 'test')
+                ->set('body', 'test')
+                ->call('sendmail');
+
+        Notification::assertSentTo($home->users, HomeMailNotification::class);
+        Notification::assertSentOnDemand(HomeMailCreatedNotification::class);
     }
 }
