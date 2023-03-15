292055ed7d264b40d55ef9c47d14adbc7bc33d8c
pop-culture-studio <pcs.engineer.team@gmail.com>
2023-03-15 13:31:59 +0900

    Update ImportCommand.php

--- app/Console/Commands/ImportCommand.php
+++ app/Console/Commands/ImportCommand.php
@@ -37,7 +37,7 @@ class ImportCommand extends Command
      *
      * @return int
      */
-    public function handle()
+    public function handle(): int
     {
         $this->info('import start...');
 
