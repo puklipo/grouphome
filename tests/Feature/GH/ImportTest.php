5d471c907b58fe3d43a259614a665b60a409972d
pop-culture-studio <pcs.engineer.team@gmail.com>
2023-03-15 14:10:03 +0900

    Update ImportTest.php

--- tests/Feature/GH/ImportTest.php
+++ tests/Feature/GH/ImportTest.php
@@ -5,6 +5,7 @@ namespace GH;
 use App\Imports\WamImport;
 use App\Jobs\ImportJob;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Maatwebsite\Excel\Imports\HeadingRowFormatter;
 use Mockery\MockInterface;
 use Tests\TestCase;
 
@@ -14,7 +15,7 @@ class ImportTest extends TestCase
 
     public function test_dispatch_import_job()
     {
-        $this->mock(WamImport::class, function (MockInterface $mock){
+        $this->mock(WamImport::class, function (MockInterface $mock) {
             $mock->shouldReceive('import')->once();
         });
 
@@ -23,10 +24,22 @@ class ImportTest extends TestCase
 
     public function test_import_import_job_fail()
     {
-        $this->mock(WamImport::class, function (MockInterface $mock){
+        $this->mock(WamImport::class, function (MockInterface $mock) {
             $mock->shouldReceive('import')->once()->andThrow(\Exception::class);
         });
 
         ImportJob::dispatch();
+    }
+
+    public function test_import_csv()
+    {
+        $this->seed();
+
+        HeadingRowFormatter::default('none');
+
+        $i = new WamImport();
+        $i->import(__DIR__.'/../../fixture/test.csv');
+
+        $this->assertDatabaseCount('homes', 2);
     }
 }
