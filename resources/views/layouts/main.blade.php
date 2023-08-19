00caaeaed5332acad75a1d98710a2fa42e883e48
pop-culture-studio <pcs.engineer.team@gmail.com>
2023-08-20 08:39:34 +0900

    Update main.blade.php

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -27,6 +27,9 @@
 
     @production
         @includeIf('layouts.ga')
+
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5908240238435723"
+     crossorigin="anonymous"></script>
     @endproduction
 </head>
 
