f369ced7b8d02fb94b1933c0aeb517a77f84ce60
pop-culture-studio <pcs.engineer.team@gmail.com>
2023-08-21 09:52:07 +0900

    Update main.blade.php

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -28,8 +28,10 @@
     @production
         @includeIf('layouts.ga')
 
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5908240238435723"
+        @cannot('admin')
+            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5908240238435723"
      crossorigin="anonymous"></script>
+        @endcannot
     @endproduction
 </head>
 
