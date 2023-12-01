be9462e8ec2865ae6b0bd5842d5e48aaa0a8a4ed
pop-culture-studio <pcs.engineer.team@gmail.com>
2023-12-01 14:39:21 +0900

    Update mail.blade.php

--- resources/views/homes/mail.blade.php
+++ resources/views/homes/mail.blade.php
@@ -1,3 +1,4 @@
+@if(filled($home->tel))
 <div class="relative">
     <div
         class="flex flex-row space-x-4 fixed bottom-1 sm:bottom-5 right-1 sm:right-5 bg-gray-500 p-6 bg-opacity-30 shadow-lg rounded-md print:hidden">
@@ -10,12 +11,11 @@
             </div>
         @endif
 
-        @isset($home->tel)
             <div class="text-lg bg-orange-500 text-white dark:bg-gray-900 dark:text-white p-3 font-bold shadow-lg rounded-lg">
                 <a href="tel:{{ $home->tel }}">
                     <x-icon.phone class="inline dark:stroke-indigo-500"/>
                     電話で問い合わせる</a>
             </div>
-        @endisset
     </div>
 </div>
+@endif
