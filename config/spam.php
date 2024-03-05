6f2a3c55e1d7a95dbd94c2a02d8a8c90c853ef1e
pop-culture-studio <pcs.engineer.team@gmail.com>
2024-03-05 10:27:38 +0900

    Create spam.php

---
+++ config/spam.php
@@ -0,0 +1,12 @@
+<?php
+
+//迷惑メール送信元はブロック
+return [
+    'hiroki_hamada@e.entrenet.jp',
+    'consulting@allegretto.co.jp',
+    'i.sales@intermind.jp',
+    'sakuraba-takeshi@propolife.co.jp',
+    'project.act@pitpa.jp',
+    'yoshida@yagish.jp',
+    'contact@nakayoshigc.com',
+];
