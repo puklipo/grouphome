56656f7519c69a02e9bb7e1abdf11a58c1097a86
pop-culture-studio <pcs.engineer.team@gmail.com>
2024-03-06 14:11:31 +0900

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('spam', function () {
+    return config('spam');
+});
