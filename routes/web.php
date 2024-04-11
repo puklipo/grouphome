97d51c9baa877ffab2cb05ae49f000af5d6078aa
pop-culture-studio <pcs.engineer.team@gmail.com>
2024-04-11 11:44:37 +0900

    wip

--- routes/web.php
+++ routes/web.php
@@ -13,17 +13,6 @@ use App\Http\Controllers\PrefController;
 use App\Http\Controllers\SitemapController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::view(uri: '/', view: 'home')->name('index');
 
 Route::resource('home', HomeController::class)
