397b55c56f29b6a85574ac0b5f0dd5921ced50ae
pop-culture-studio <pcs.engineer.team@gmail.com>
2024-04-30 12:56:15 +0900

    Update console.php

--- routes/console.php
+++ routes/console.php
@@ -11,9 +11,3 @@ Artisan::command('inspire', function () {
 Artisan::command('sitemap', function () {
     SitemapJob::dispatch();
 })->purpose('Create sitemap');
-
-Artisan::command('csv:convert', function () {
-    $csv = file_get_contents(resource_path('csv/wam_sjis.csv'));
-    $csv = mb_convert_encoding($csv, 'UTF-8', 'SJIS');
-    file_put_contents(resource_path('csv/wam_utf8.csv'), $csv);
-})->purpose('元のCSVがShift-JISだった場合にUTF-8に変換');
