7231e1fa79d732cfb6341d7c284931c4b1346867
puklipo <pcs.engineer.team@gmail.com>
2025-10-03 23:09:46 +0900

    Update basic-editor.blade.php

--- resources/views/livewire/home/basic-editor.blade.php
+++ resources/views/livewire/home/basic-editor.blade.php
@@ -11,16 +11,6 @@
                              wire:model="form.released_at"></x-input>
                 <div class="text-sm text-gray-500 dark:text-white mb-3">指定年月日。</div>
 
-                <x-label for="wam" value="{{ __('WAM URL') }}"/>
-                <x-input type="text" name="wam" id="wam" wire:model="form.wam" class="w-full"></x-input>
-                <div class="text-sm text-gray-500 dark:text-white mb-3">WAM
-                    URLもしくは公式とは別のURLを入力。CSVインポートで上書きされないURLとして利用。
-                </div>
-
-                <x-label for="map" value="{{ __('Googleマップ') }}"/>
-                <x-textarea name="map" id="map" rows="2" wire:model="form.map"></x-textarea>
-                <div class="text-sm text-gray-500 dark:text-white mb-3">埋め込み用のHTML。（管理事業者でも自由にhtml入力可能にはできないので管理者専用）</div>
-
                 <x-label for="area" value="{{ __('住所（自治体のみ）') }}"/>
                 <x-input type="text" name="area" id="area" wire:model="form.area" class="w-full"></x-input>
                 <div class="text-sm text-gray-500 dark:text-white mb-3">市区町村までの住所。</div>
@@ -28,6 +18,16 @@
                 <x-label for="address" value="{{ __('住所（番地まで全部）') }}"/>
                 <x-input type="text" name="address" id="address" wire:model="form.address" class="w-full"></x-input>
                 <div class="text-sm text-gray-500 dark:text-white mb-3">番地まで含めた完全な住所。</div>
+
+                <x-label for="wam" value="{{ __('WAM URL') }}"/>
+                <x-input type="text" name="wam" id="wam" wire:model="form.wam" class="w-full"></x-input>
+                <div class="text-sm text-gray-500 dark:text-white mb-3">WAM
+                    URLもしくは公式とは別のURLを入力。CSVインポートで上書きされないURLとして利用。
+                </div>
+
+                <x-label for="map" value="{{ __('Googleマップ') }}"/>
+                <x-textarea name="map" id="gmap" rows="2" wire:model="form.map"></x-textarea>
+                <div class="text-sm text-gray-500 dark:text-white mb-3">埋め込み用のHTML。（管理事業者でも自由にhtml入力可能にはできないので管理者専用）</div>
 
                 <x-button class="mt-3">
                     {{ __('更新') }}
